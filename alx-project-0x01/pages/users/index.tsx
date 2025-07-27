18f415b847ad36938fdbf6bd72c03467b43cc088
akka-tech <akkaabdlwahd3@gmail.com>
2025-07-27 23:50:20 +0200

    feat: implement user listing with UserCard component and fetch user data

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,31 @@
+// pages/landing/index.tsx (or another appropriate route)
 import React from 'react'
+import UserCard from '@/components/common/UserCard'
+import { UserProps } from '@/interfaces'
 
-export default function indes() {
+interface Props {
+  posts: UserProps[]
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default function IndexPage({ posts }: Props) {
   return (
-    <div>indes</div>
+    <div className="p-6 space-y-4">
+      {posts.map(({ id, name, email, phone }) => (
+        <div key={id} className="p-4 border rounded-md shadow-sm">
+          <UserCard name={name} email={email} username={phone} id={id} />
+        </div>
+      ))}
+    </div>
   )
 }
